1355a3b1a0615ce6f228d44fb97da084177f2ef6
lhreczuch <141577513+lhreczuch@users.noreply.github.com>
2025-03-01 18:36:55 +0100

    Update settings.py

--- car_fix_control_system/car_fix_control_system/settings.py
+++ car_fix_control_system/car_fix_control_system/settings.py
@@ -200,4 +200,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'carfixserwis@gmail.com'
-EMAIL_HOST_PASSWORD = "mtrbwnfahdswvmnh
+EMAIL_HOST_PASSWORD = "mtrbwnfahdswvmnh"
